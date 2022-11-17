abc6f8e662f6aae4c7e4ef15a2b0bfa2d52f39f5
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-17 09:09:10 -0800

    Bumping version 3.0.0 (#54)
    * better cli output
    
    * updating documentation
    
    * updating documentation
    
    * updating badge
    
    * updating changelog
    
    * Update publish.yml
    
    * updating setup.py
    
    * Update README.md
    
    * first stable release
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * Update CHANGELOG.md
    
    * updating cli
    
    * refactoring code
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * fixing flake8 error
    
    * bug fixed
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * tested
    
    * flake8 error
    
    * flake8 error
    
    * refactroing code
    
    * updating docs
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * test cases updated
    
    * test cases updated
    
    * adding scripts for release automation (#49)
    
    * bumping version 1.0.0 to 2.0.0 (#50)
    
    * adding scripts for release automation
    
    * v2.0.0 releasing
    
    * updating changelog
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * Delete update_package.sh
    
    * Update FUNDING.yml
    
    * Update FUNDING.yml
    
    * Issue #44 (#52)
    
    * add gender options for first names
    
    * add gender options for first names, random card
    
    * add job experience according to years of age
    
    * add parameter option for logging (not implemented yet)
    
    * add handpicked locations to cities in list
    
    * add float coords generation
    
    * add coordinates string formatting
    
    * fix ages in job level generation
    
    * start of unit tests
    
    * add more tests
    
    * add assertregex ussage in tests
    
    * fix missing imports
    
    * lint fixes
    
    * two lines padding before class
    
    * solving git conflict
    
    * refactoring code
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    
    * refacoting code and changelog update
    
    * Update FUNDING.yml
    
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
    Co-authored-by: Rahul Sharma <53327705+DrakeEntity@users.noreply.github.com>
    Co-authored-by: DavidCano98 <63115730+DavidCano98@users.noreply.github.com>

--- random_profile/main.py
+++ random_profile/main.py
@@ -6,43 +6,67 @@ github : codeperfectplus
 '''
 
 import os
+import sys
 import uuid
 import random
 from typing import List, Tuple
 
-from random_profile.utils import ipv4_gen
-from random_profile.utils import load_txt_file
-from random_profile.utils import generate_dob_age
-from random_profile.utils import generate_random_height_weight
-from random_profile.utils import ASSETS_DIR
+sys.path.append('.')
 
-VERSION = '2.0.0'
+from random_profile.enums.gender import Gender
+from random_profile import utils
 
-fname_txt = os.path.join(ASSETS_DIR, 'fnames.txt')
-lname_txt = os.path.join(ASSETS_DIR, 'lnames.txt')
-hair_colors_txt = os.path.join(ASSETS_DIR, 'hair_colors.txt')
-blood_types_txt = os.path.join(ASSETS_DIR, 'blood_types.txt')
-street_names_txt = os.path.join(ASSETS_DIR, 'street_names.txt')
-cities_name_txt = os.path.join(ASSETS_DIR, 'cities_name.txt')
-states_names_txt = os.path.join(ASSETS_DIR, 'states_names.txt')
-job_titles_txt = os.path.join(ASSETS_DIR, 'job_titles.txt')
-states_hash_json = os.path.join(ASSETS_DIR, 'states_hash.json')
+VERSION = '3.0.0'
+
+lname_txt = os.path.join(utils.ASSETS_DIR, "lnames.txt")
+fname_male_txt = os.path.join(utils.ASSETS_DIR, "fnames_male.txt")
+fname_female_txt = os.path.join(utils.ASSETS_DIR, "fnames_female.txt")
+hair_colors_txt = os.path.join(utils.ASSETS_DIR, "hair_colors.txt")
+blood_types_txt = os.path.join(utils.ASSETS_DIR, "blood_types.txt")
+street_names_txt = os.path.join(utils.ASSETS_DIR, "street_names.txt")
+cities_name_txt = os.path.join(utils.ASSETS_DIR, "cities_name.txt")
+states_names_txt = os.path.join(utils.ASSETS_DIR, "states_names.txt")
+job_titles_txt = os.path.join(utils.ASSETS_DIR, "job_titles.txt")
+job_levels_txt = os.path.join(utils.ASSETS_DIR, "job_levels.txt")
 
 # loading data from txt files
-fname = load_txt_file(fname_txt)
-lname = load_txt_file(lname_txt)
-hair_colors = load_txt_file(hair_colors_txt)
-blood_types = load_txt_file(blood_types_txt)
-states_names = load_txt_file(states_names_txt)
-cities_name = load_txt_file(cities_name_txt)
-street_names = load_txt_file(street_names_txt)
-job_titles = load_txt_file(job_titles_txt)
+lname = utils.load_txt_file(lname_txt)
+fname_male = utils.load_txt_file(fname_male_txt)
+fname_female = utils.load_txt_file(fname_female_txt)
+hair_colors = utils.load_txt_file(hair_colors_txt)
+blood_types = utils.load_txt_file(blood_types_txt)
+states_names = utils.load_txt_file(states_names_txt)
+cities_name = utils.load_txt_file(cities_name_txt)
+street_names = utils.load_txt_file(street_names_txt)
+job_titles = utils.load_txt_file(job_titles_txt)
+job_levels = utils.load_txt_file(job_levels_txt)
 
 
 class RandomProfile(object):
-    ''' Random Profile Generator '''
-    def __init__(self, num: int = 1):
+    """ Random Profile Generator
+
+    Args:
+        num (int, optional): Total No. of Name You Want To Print. Defaults to 1.
+        gender(str, optional): default is None. if you want to generate define gender then pass
+
+    Methods:
+        full_profiles: Generate Full Profile
+        first_names: Generate First Name
+        last_names: Generate Last Name
+        full_names: Generate Full Name
+        email: Generate Email
+        phone_number: Generate Phone Number
+        dob_age: Generate Date of Birth and Age
+        height_weight: Generate Height and Weight
+        address: Generate Address
+        ip_address: Generate IP Address
+        hair_color: Generate Hair Color
+        blood_type: Generate Blood Type
+        job_title: Generate Job Title
+    """
+    def __init__(self, num: int = 1, gender: Gender = None):
         self.num = num
+        self.gender = gender
 
     def __str__(self) -> str:
         return f'Random Profile Generator version {VERSION}'
@@ -65,29 +89,11 @@ class RandomProfile(object):
     def __getitem__(self, index):
         return self.full_profile()[index]
 
-    def first_name(self, num: int = None) -> List[str]:
-        num = self.num if num is None else num
-        if num == 1 or num is None:
-            return random.choice(fname)
-        return random.choices(fname, k=num)
-
-    def last_name(self, num: int = None) -> list:
-        num = self.num if num is None else num
-        if num == 1 or num is None:
-            return random.choice(lname)
-        return random.choices(lname, k=num)
-
-    def full_name(self, num: int = None) -> List[str]:
-        num = self.num if num is None else num
-        if num == 1 or num is None:
-            return f'{random.choice(fname)} {random.choice(lname)}'
-        return [random.choice(fname) + ' ' + random.choice(lname) for _ in range(num)]
-
     def ip_address(self, num: int = None) -> List[str]:
         num = self.num if num is None else num
         if num == 1 or num is None:
-            return ipv4_gen()
-        return [ipv4_gen() for _ in range(num)]
+            return utils.ipv4_gen()
+        return [utils.ipv4_gen() for _ in range(num)]
 
     def job_title(self, num: int = None) -> List[str]:
         num = self.num if num is None else num
@@ -110,14 +116,14 @@ class RandomProfile(object):
     def dob_age(self, num: int = None) -> List[Tuple[str, int]]:
         num = self.num if num is None else num
         if num == 1 or num is None:
-            return generate_dob_age()
-        return [generate_dob_age() for _ in range(num)]
+            return utils.generate_dob_age()
+        return [utils.generate_dob_age() for _ in range(num)]
 
     def height_weight(self, num: int = None) -> List[Tuple[int, int]]:
         num = self.num if num is None else num
         if num == 1 or num is None:
-            return generate_random_height_weight()
-        return [generate_random_height_weight() for _ in range(num)]
+            return utils.generate_random_height_weight()
+        return [utils.generate_random_height_weight() for _ in range(num)]
 
     def generate_address(self, num: int = None) -> List[str]:
         num = self.num if num is None else num
@@ -140,37 +146,118 @@ class RandomProfile(object):
 
         return address_list
 
-    def full_profile(self, num: int = None) -> List[dict]:
+    def first_names(self, num: int = None, gender: Gender = None) -> list:
         num = self.num if num is None else num
+        gender = self.gender if gender is None else gender
+
+        # DRY CODE
+        if gender is None:
+            names = fname_female + fname_male
+        elif gender.value == Gender.MALE.value:
+            names = fname_male
+        else:
+            names = fname_female
+
+        if num == 1 or num is None:
+            return random.choice(names)
+
+        return random.choices(names, k=num)
+
+    def last_names(self, num: int = None) -> list:
+        num = self.num if num is None else num
+        if num == 1 or num is None:
+            return random.choice(lname)
+
+        return random.choices(lname, k=num)
+
+    def full_names(self, num: int = None, gender: Gender = None) -> list:
+        num = self.num if num is None else num
+        gender = self.gender if gender is None else gender
+
+        if gender is None:
+            names = fname_female + fname_male
+        elif gender.value == Gender.MALE.value:
+            names = fname_male
+        else:
+            names = fname_female
+
+        if num == 1 or num is None:
+            return random.choice(names) + ' ' + random.choice(lname)
+
+        return [random.choice(names) + ' ' + random.choice(lname) for _ in range(num)]
+
+    def full_profiles(self, num: int = None, gender: Gender = None) -> list:
+        num = self.num if num is None else num
+
         profile_list = []
+
         for _ in range(num):
+            # random gender for every profile in list
+            this_gender = utils.generate_random_gender() if gender is None else gender
+            first = random.choice(fname_male if this_gender.value == Gender.MALE.value else fname_female)
+            last = random.choice(lname)
+            full_name = first + ' ' + last
+
+            hair_color = random.choice(hair_colors)
+            blood_type = random.choice(blood_types)
+
             phone_number = f'+1-{random.randint(300, 500)}-{random.randint(800, 999)}-{random.randint(1000,9999)}'
-            dob, age = self.dob_age(num=1)
-            height, weight = self.height_weight(num=1)
-            address = self.generate_address()[0]
-            full_address = '{} {} {}, {} {}'.format(address['street_num'],
-                                                    address['street'],
-                                                    address['city'],
-                                                    address['state'],
-                                                    address['zip_code'])
+
+            dob, age = utils.generate_dob_age()
+            height, weight = utils.generate_random_height_weight()
+            job_experience = utils.generate_random_job_level(age, job_levels)
+
+            street_num = random.randint(100, 999)
+            street = random.choice(street_names)
+            city, coords = utils.generate_random_city_coords(cities_name)
+            coords_pretty = utils.coords_string(coords)
+            state = random.choice(states_names)
+            zip_code = random.randint(10000, 99999)
+
+            address = {
+                'street_num': street_num,
+                'street': street,
+                'city': city,
+                'state': state,
+                'zip_code': zip_code
+            }
+
+            full_address = f'{street_num} {street}, {city}, {state} {zip_code}'
+
+            mother = self.first_names(1, Gender.FEMALE)[0] + ' ' + last
+            father = self.first_names(1, Gender.MALE)[0] + ' ' + last
+
+            card = utils.generate_random_card()
 
             profile = {}
             profile['id'] = str(uuid.uuid4())
-            profile['first_name'] = self.first_name(num=1)
-            profile['last_name'] = self.first_name(num=1)
-            profile['full_name'] = profile['first_name'] + ' ' + profile['last_name']
+            profile['gender'] = this_gender.value
+
+            profile['first_name'] = first
+            profile['last_name'] = last
+            profile['hair_color'] = hair_color
+            profile['blood_type'] = blood_type
+            profile['full_name'] = full_name
+
             profile['job_title'] = self.job_title(num=1)
             profile['dob'] = dob
             profile['age'] = age
             profile['phone_number'] = phone_number
             profile['email'] = profile['first_name'].lower() + profile['last_name'].lower() + '@example.com'
-            profile['address'] = address
-            profile['full_address'] = full_address
+
             profile['blood_type'] = self.blood_type(num=1)
             profile['height'] = height
             profile['weight'] = weight
             profile['hair_color'] = self.hair_color(num=1)
             profile['ip_address'] = self.ip_address(num=1)
+
+            profile['address'] = address
+            profile['full_address'] = full_address
+            profile['job_job_experience'] = job_experience
+            profile['mother'] = mother
+            profile['father'] = father
+            profile['payment_card'] = card
+            profile['coordinates'] = coords_pretty
 
             profile_list.append(profile)
 
